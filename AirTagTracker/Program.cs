9d9b666a937bfbbe08fe906821b4c0ffe533c286
Michael Clark <clarkpmike@gmail.com>
2022-10-09 21:26:19 -0700

    Added try-catch functionality

--- AirTagTracker/Program.cs
+++ AirTagTracker/Program.cs
@@ -1,27 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AirTagTracker;
 
-Cache _cache;
+Cache? _cache = null;
+
+List<Cache> _failedUploads = new List<Cache>();
+
+string filepath = @"C:\Users\clark\Downloads\Items.data";
 
 /// Can I make this run on startup in MacOS?
 /// Open the Find My App (if Possible)
 // Log that Find My is open (if Possible) Something like system.threading.tasks or TaskManager
 
-Console.WriteLine("Reading Cache from Dir");
-// Read the cahce from the directory
-// ~/Library/Cache/Apps... on Mac
-// Some temp location on PC   "C:\Users\clark\Downloads\Items.data"
-using (StreamReader reader = File.OpenText(@"C:\Users\clark\Downloads\Items.data"))
+// Infinite Loop
+while (true)
 {
-    var text = reader.ReadToEnd();
+    try
+    {
+        Console.WriteLine("Reading Cache from Dir");
+        // Read the cahce from the directory
+        // ~/Library/Cache/Apps... on Mac
+        // Some temp location on PC   "C:\Users\clark\Downloads\Items.data"
 
-    _cache = new Cache(text);
+        using (StreamReader reader = File.OpenText(filepath))
+        {
+            var text = reader.ReadToEnd();
+
+            _cache = new Cache(text, filepath);
+        }
+
+
+        Console.WriteLine("Writing Cache to DB");
+        // Write the latest Cache data to the database
+        CacheUploader.Upload(_cache);
+        _cache = null;
+
+    }
+
+    catch (Exception)
+    {
+
+        if (_cache != null) _failedUploads.Add(_cache);
+
+        throw;
+    }
+
+    // Repeat every 15 minutes 
+    Console.WriteLine("Waiting for repeat");
+    Thread.Sleep(15 * 60000);
 }
-
-
-Console.WriteLine("Writing Cache to DB");
-// Write the latest Cache data to the database
-CacheUploader.Upload(_cache);
-
-Console.WriteLine("Waiting for repeat");
-// Repeat every 15 minutes 
