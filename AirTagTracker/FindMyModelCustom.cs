1caceb0679beeb032d398c7778831c4dbfbbac2d
michaeltclark <clarkpmike@gmail.com>
2022-10-16 17:17:47 -0700

    Configuration Manager

--- AirTagTracker/FindMyModelCustom.cs
+++ AirTagTracker/FindMyModelCustom.cs
@@ -1,11 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AirTagTracker
 {
@@ -19,8 +12,7 @@ namespace AirTagTracker
 
         public static void ConfigureOptions(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=tcp:granitetestingground.database.windows.net,1433;Initial Catalog=airtags;Persist Security Info=False;User ID=GraniteAdmin;Password=mBnx$Ddqc$yP9wR;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionString);
         }
-
     }
 }
